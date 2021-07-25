1e2d6efcd0c4d4242812cc1523e7c1bedf0e29cd
whereisthree <63399861+whereisthree@users.noreply.github.com>
2021-07-25 06:20:42 -0400

    Update como_route.user.js

--- como_route.user.js
+++ como_route.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         COMO - Show Routes
 // @namespace    mailto:jeyartil@amazon.com
-// @version      0.3
+// @version      0.35
 // @description  Show Routes
 // @author       jeyartil
 // @match        https://como-operations-dashboard-iad.iad.proxy.amazon.com/store/f170be3c-eda4-43dd-b6bd-2325b4d3c719/jobdetails?jobId=*
@@ -51,7 +51,7 @@
 			switch (temp) {
 			}
 
-			cells.temp = temp && console.log(root);
+			cells.temp;
 
 			cells.lastKnownLocation.innerHTML =
 				lastKnownLocation && lastKnownLocation !== cells.lastKnownLocation.innerText

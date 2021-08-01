5856a670a55a4215772bff584f2390c22c6d7ad4
Jeyson Artiles <JeysonArtiles@users.noreply.github.com>
2021-08-01 10:58:43 -0400

    Update findBags_aftlite.user.js

--- findBags_aftlite.user.js
+++ findBags_aftlite.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         [ FIND BAGS ] AFTLITE
 // @namespace    mailto:jeyartil@amazon.com
-// @version      0.1
+// @version      0.2
 // @description  Get Routes
 // @author       jeyartil
 // @match        https://aftlite-na.amazon.com/wms/*
 // @downloadURL  https://raw.githubusercontent.com/JeysonArtiles/amzn/master/findBags_aftlite.user.js
 // @updateURL    https://raw.githubusercontent.com/JeysonArtiles/amzn/master/findBags_aftlite.user.js
-// @icon         https://www.google.com/s2/favicons?domain=amazon.com
+// @icon         https://www.google.com/s2/favicons?domain=amazon.com-update-test
 // @grant        none
 // ==/UserScript==
 

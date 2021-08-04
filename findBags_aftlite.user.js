c1f717399e967efd2b4f56815c829827fbdce418
Jeyson Artiles <JeysonArtiles@users.noreply.github.com>
2021-08-04 01:49:19 -0400

    escape prompt

--- findBags_aftlite.user.js
+++ findBags_aftlite.user.js
@@ -18,6 +18,7 @@
     document.onkeyup = function (e) {
         if (e.shiftKey && e.which == 70) {
             const TRACKING_NUMBER = prompt("ENTER TRACKING NUMBER:");
+		if (TRACKING_NUMBER == null) return;
             alert("CONFIRM ACTIVE SESSION BY RELOADING PAGE. THIS MAY TAKE SOME TIME...PRESS ENTER.");
             findMissingBags(TRACKING_NUMBER);
         }

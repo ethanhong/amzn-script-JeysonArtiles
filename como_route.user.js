748db7b577ee7f72bed3972b12bf7e4e80471df2
Jeyson Artiles <JeysonArtiles@users.noreply.github.com>
2021-08-06 16:04:29 -0400

    shortcut search

--- como_route.user.js
+++ como_route.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         COMO - Show Routes
 // @namespace    mailto:jeyartil@amazon.com
-// @version      0.7
+// @version      0.6
 // @description  Show Routes
 // @author       jeyartil
 // @match        https://como-operations-dashboard-iad.iad.proxy.amazon.com/store/*
@@ -11,65 +11,68 @@
 // @updateURL  	 https://raw.githubusercontent.com/JeysonArtiles/amzn/master/como_route.user.js
 // ==/UserScript==
 
-(function (history) {
-	var pushState = history.pushState;
-	history.pushState = function (state) {
-		setTimeout(() => getPackageDetails(), 1203);
-		return pushState.apply(history, arguments);
-	};
+(function(history){
+    var pushState = history.pushState;
+    history.pushState = function(state) {
+        setTimeout(() => getPackageDetails(), 1203);
+      return pushState.apply(history, arguments);
+    };
 })(window.history);
 
 if (window.location.href.includes("jobId")) {
-	// SHORTCUTS
-	document.onkeyup = function (e) {
-		if (e.shiftKey && e.which == 84) {
-			checkPackage();
-			console.log("job");
-		}
-	};
+    // SHORTCUTS
+    document.onkeyup = function (e) {
+        if (e.shiftKey && e.which == 84) {
+            checkPackage();
+            console.log("job");
+        }
+    };
 }
 
 const getPackageDetails = () => {
-	let packages = [...document.querySelectorAll("tr[ng-repeat='pkg in ctrl.packages']")];
+    let packages = [
+        ...document.querySelectorAll("tr[ng-repeat='pkg in ctrl.packages']")
+    ];
 
-	console.log(packages);
+    console.log(packages)
 
-	const activeTasks = JSON.parse(sessionStorage.activeTasks);
+    const activeTasks = JSON.parse(sessionStorage.activeTasks);
 
-	const lastKnownLocationSpan = (lastKnownLocation) =>
-		`<span style="background-color:#555555; color: white; padding: 5px; font-weight: bold; text-align: center; min-width: 175px; display: inline-block">${lastKnownLocation}</span>`;
+    const lastKnownLocationSpan = (lastKnownLocation) => `<span style="background-color:#555555; color: white; padding: 5px; font-weight: bold; text-align: center; min-width: 175px; display: inline-block">${lastKnownLocation}</span>`;
 
-	const matchedPackagesinnerHTML = [];
+    const matchedPackagesinnerHTML = [];
 
-	packages.map((pkg) => {
-		let cells = {};
-		cells.number = pkg.cells[1];
-		cells.status = pkg.cells[2];
-		cells.scannableId = pkg.cells[3];
-		cells.temp = pkg.cells[4];
-		cells.lastKnownLocation = pkg.cells[5];
-		cells.lastModifiedTime = pkg.cells[6];
-		cells.order = pkg.cells[7];
+    packages.map((pkg) => {
+        let cells = {};
+        cells.number = pkg.cells[1];
+        cells.status = pkg.cells[2];
+        cells.scannableId = pkg.cells[3];
+        cells.temp = pkg.cells[4];
+        cells.lastKnownLocation = pkg.cells[5];
+        cells.lastModifiedTime = pkg.cells[6];
+        cells.order = pkg.cells[7];
 
-		activeTasks.map((ssp) => {
-			ssp.map((packages) => {
-				if (cells.scannableId.innerText == packages.scannableId) {
-					cells.lastKnownLocation.innerHTML =
-						cells.lastKnownLocation.innerText !== packages.handoffLocation
-							? `${cells.lastKnownLocation.innerText} ${lastKnownLocationSpan(
-									packages.handoffLocation
-							  )}`
-							: cells.lastKnownLocation.innerHTML;
-				}
-			});
-		});
-	});
-};
+        activeTasks.map(ssp => {
+            ssp.map((packages) => {
+                if (cells.scannableId.innerText == packages.scannableId) {
+                    //console.log(cells.lastKnownLocation, packages.handoffLocation)
+                    cells.lastKnownLocation.innerHTML = cells.lastKnownLocation.innerText !== packages.handoffLocation ? `${cells.lastKnownLocation.innerText} ${lastKnownLocationSpan(packages.handoffLocation)}`
+					 : cells.lastKnownLocation.innerHTML;
+                    }
+                })
+            })
+        });
+    };
 
-//setTimeout(() => getPackageDetails(), 2000);
+setTimeout(() => getPackageDetails(), 2000);
 
 const checkPackage = () => {
-	let pkg = prompt("Enter tracking code");
-	//alert(JSON.parse(sessionStorage[pkg]).handoffLocation);
-	alert(JSON.parse(sessionStorage[pkg]).handoffLocation);
+    let pkg = prompt("Enter tracking code");
+    //alert(JSON.parse(sessionStorage[pkg]).handoffLocation);
+    const allPackages = JSON.parse(sessionStorage.packages);
+
+    const result = allPackages.find(package => package.scannableId == pkg);
+
+    alert(result.handoffLocation)
+
 };

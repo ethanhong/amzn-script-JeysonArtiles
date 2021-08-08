6692df7d95ed3dc630651674745320e32790dcb2
Jeyson Artiles <JeysonArtiles@users.noreply.github.com>
2021-08-08 06:17:05 -0400

    batching time next to ppst

--- batchingMonitor_como.user.js
+++ batchingMonitor_como.user.js
@@ -12,68 +12,103 @@
 // @require      https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
 // ==/UserScript==
 
-(function() {
-    'use strict';
 
-    sessionStorage.tasksPerBatcher = 4
+sessionStorage.tasksPerBatcher = 4
 
-    hotkeys('shift+b', function() {
-        const tasksPerBatcher = prompt("Enter desired tasks per batcher:", 4);
-        if(!tasksPerBatcher) return;
-        setTasksPerBatcher(tasksPerBatcher);
-    });
+hotkeys('shift+b', function() {
+    const tasksPerBatcher = prompt("Enter desired tasks per batcher:", 4);
+    if(!tasksPerBatcher) return;
+    setTasksPerBatcher(tasksPerBatcher);
+});
 
-    const setTasksPerBatcher = (number) => { sessionStorage.tasksPerBatcher = number; };
+const setTasksPerBatcher = (number) => { sessionStorage.tasksPerBatcher = number; };
 
-    setTimeout(() => {
-        updateTasks();
-    }, 500);
+setTimeout(() => {
+    updateTasks();
+}, 500);
 
-    const fetchData = async () => {
-        const response = await fetch(
-            "https://como-operations-dashboard-iad.iad.proxy.amazon.com/api/store/f170be3c-eda4-43dd-b6bd-2325b4d3c719/activeJobSummary"
-        );
+const fetchData = async () => {
+    const response = await fetch(
+        "https://como-operations-dashboard-iad.iad.proxy.amazon.com/api/store/f170be3c-eda4-43dd-b6bd-2325b4d3c719/activeJobSummary"
+    );
 
-        const data = await response.json();
-        const tasks = {};
-        tasks.total = data.length;
-        tasks.inProgress = data.filter(({operationState}) => operationState == "IN_PROGRESS").length;
-        tasks.current = data.filter(({operationState}) => operationState == "NONE" || operationState == "IN_PROGRESS").length;
-        tasks.partial = data.filter(({operationState, fulfillmentComplete}) => operationState == "COMPLETED" && fulfillmentComplete == false).length;
-        tasks.complete = data.filter(({operationState}) => operationState == "COMPLETED").length - tasks.partial;
+    const data = await response.json();
+    const tasks = {};
+    tasks.total = data.length;
+    tasks.inProgress = data.filter(({operationState}) => operationState == "IN_PROGRESS").length;
+    tasks.current = data.filter(({operationState}) => operationState == "NONE" || operationState == "IN_PROGRESS").length;
+    tasks.partial = data.filter(({operationState, fulfillmentComplete}) => operationState == "COMPLETED" && fulfillmentComplete == false).length;
+    tasks.complete = data.filter(({operationState}) => operationState == "COMPLETED").length - tasks.partial;
 
-        sessionStorage.tasks = JSON.stringify(tasks);
+    sessionStorage.tasks = JSON.stringify(tasks);
 
-        //console.log(sessionStorage.tasks)
-    }
+    //console.log(sessionStorage.tasks)
+}
 
-    setInterval(() => { fetchData(); updateTasks(sessionStorage.tasks); }, 1000);
-    let debugUpdating = 0;
+setInterval(() => { fetchData(); updateTasks(sessionStorage.tasks); }, 1000);
 
-    const updateTasks = (task) => {
-        const tasks = JSON.parse(task);
+let debugUpdating = 0;
+const updateTasks = (task) => {
+    const tasks = JSON.parse(task);
 
-        const DOM = {};
-        DOM.tasks = document.querySelector("h1[data-dtk-test-id='job-grid-title']");
-        let recommendedBatchers = (tasks.current / sessionStorage.tasksPerBatcher).toFixed(2);
+    const DOM = {};
+    DOM.tasks = document.querySelector("h1[data-dtk-test-id='job-grid-title']");
+    let recommendedBatchers = (tasks.current / sessionStorage.tasksPerBatcher).toFixed(2);
 
-        //if((tasks.total - tasks.current) / sessionStorage.tasksPErBatcher > tasks.current) recommendedBatchers = (tasks.current / sessionStorage.tasksPerBatcher).toFixed(1);
+    //if((tasks.total - tasks.current) / sessionStorage.tasksPErBatcher > tasks.current) recommendedBatchers = (tasks.current / sessionStorage.tasksPerBatcher).toFixed(1);
 
-        //if (recommendedBatchers > tasks.inProgress) recommendedBatchers = tasks.current;
-        if (recommendedBatchers < tasks.inProgress) recommendedBatchers = tasks.inProgress;
+    //if (recommendedBatchers > tasks.inProgress) recommendedBatchers = tasks.current;
+    if (recommendedBatchers < tasks.inProgress) recommendedBatchers = tasks.inProgress;
 
-        debugUpdating++;
-        DOM.tasks.innerHTML = `Tasks (${tasks.current})
+    debugUpdating++;
+    DOM.tasks.innerHTML = `Tasks (${tasks.current})
             <span id="recommendedBatchers" style="margin-left: 1em">BATCHERS:
             <span id="batchers">${tasks.inProgress} / ${recommendedBatchers}</span></span>
             <span id="action"></span> <span style="margin: 1em" hidden>Update Debugger: ${debugUpdating}</span>`;
 
-        DOM.recommendedBatchers = document.querySelector('#recommendedBatchers');
-        DOM.batchers = document.querySelector('#batchers');
+    DOM.recommendedBatchers = document.querySelector('#recommendedBatchers');
+    DOM.batchers = document.querySelector('#batchers');
 
-        DOM.action = document.querySelector('#action');
+    DOM.action = document.querySelector('#action');
 
-        if(tasks.inProgress == 0) DOM.recommendedBatchers.style.visibility = "hidden";
+    if(tasks.inProgress == 0) DOM.recommendedBatchers.style.visibility = "hidden";
+}
+
+const batchingTime = (allRoutes) => {
+    const routes = JSON.parse(allRoutes);
+    const inProgressRoutes = routes.inProgress;
+
+    const DOM = {};
+    DOM.batchingTasks = [...document.querySelectorAll(`job-card.job-card-container`)];
+
+    const batchingTasks = DOM.batchingTasks.map(tasks => {
+        const children = tasks.children[0].children;
+        //children[0].innerHTML = "test"
+        const cells = {};
+        cells.jobId = { root: children[0] };
+        cells.jobId.value = cells.jobId.root.innerText.trim()
+        cells.destination = { root: children[1] };
+        cells.destination.value = cells.destination.root.innerText.trim();
+        cells.carts = { root: children[2] };
+        cells.carts.value = cells.carts.root.innerText.trim();
+        cells.batcher = { root: children[3] };
+        cells.batcher.value = cells.batcher.root.innerText.trim();
+        cells.ppst = { root: children[4] };
+        cells.ppst.value = cells.ppst.root.innerText.trim();
+
+        return cells
+    })
+
+    const matchedTasksInProgress = inProgressRoutes.map(route => {
+        const matchedTasks = batchingTasks.find(task => route.associateId == task.batcher.value);
+        const matchedRoutes = inProgressRoutes.find(route => route.associateId == matchedTasks.batcher.value);
+
+        const ppstSpan = matchedTasks.ppst.root.children[0].children[1];
+        ppstSpan.innerText = `${matchedTasks.ppst.root.innerText.split(" ( ")[0]} ( ${matchedRoutes.batchingTime.toFixed(2)} )`;
+
+        return matchedTasks
+    })
+
     }
 
-    })();
+setInterval(() => batchingTime(sessionStorage.allRoutes), 100);

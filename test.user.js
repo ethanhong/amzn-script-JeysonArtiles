bf97342b4a79638d817ab29fe415fd543a232575
Jeyson Artiles <JeysonArtiles@users.noreply.github.com>
2021-08-11 18:45:42 -0400

    Create test.user.js

---
+++ test.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         [ BATCHING MONITOR ] COMO
+// @namespace    https://github.com/JeysonArtiles/amzns
+// @version      2.0
+// @description  MONITOR BATCHING TASKS: SET RECOMMENDED BATCHERS. SET TASKS PER BATCHER. SHOW ELAPSED TIME (ET) PER TASK W/ 40 SEC BUFFER.
+// @author       You
+// @match        https://como-operations-dashboard-iad.iad.proxy.amazon.com/store/*/dash
+// @downloadURL  https://raw.githubusercontent.com/JeysonArtiles/amzn/master/batchingMonitor_como.user.js
+// @updateURL    https://raw.githubusercontent.com/JeysonArtiles/amzn/master/batchingMonitor_como.user.js
+// @icon         https://www.google.com/s2/favicons?domain=amazon.com-update-test
+// @grant        none
+// @require      https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
+// ==/UserScript==

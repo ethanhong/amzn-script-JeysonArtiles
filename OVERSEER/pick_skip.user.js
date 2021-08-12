43237c41d66d748381a8b5d4bf878671703aa4d8
Jeyson Artiles <JeysonArtiles@users.noreply.github.com>
2021-08-12 05:22:22 -0400

    https

--- OVERSEER/pick_skip.user.js
+++ OVERSEER/pick_skip.user.js
@@ -4,7 +4,7 @@
 // @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @match        http://*/*
+// @match        https://*/*
 // @icon         https://www.google.com/s2/favicons?domain=amazon.com
 // @downloadURL  https://raw.githubusercontent.com/JeysonArtiles/amzn/master/OVERSEER/pick_skip.user.js
 // @grant        GM_xmlhttpRequest

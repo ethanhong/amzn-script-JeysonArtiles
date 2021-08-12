ef2325c838b765fd87c6b8f3af6394b5b148d4bd
Jeyson Artiles <JeysonArtiles@users.noreply.github.com>
2021-08-12 04:56:11 -0400

    init

---
+++ OVERSEER/pick_skip.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         [ OVERSEER ] LIVE PICK SKIP
+// @namespace    https://github.com/JeysonArtiles/amzn
+// @version      0.1
+// @description  TRACK PICK SKIPS IN REAL TIME.
+// @author       You
+// @match        https://*/*
+// @downloadURL  https://raw.githubusercontent.com/JeysonArtiles/amzn/master/overseer/pick_skip.user.js
+// @icon         https://www.google.com/s2/favicons?domain=amazon.com-update-test
+// @grant        GM_xmlhttpRequest
+// @connect      chime.aws
+// @/require     https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
+// ==/UserScript==

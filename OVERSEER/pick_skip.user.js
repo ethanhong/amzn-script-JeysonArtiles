ad8a4ee3369ada1f1da01f7c5daa94ac01ad424d
Jeyson Artiles <JeysonArtiles@users.noreply.github.com>
2021-08-12 05:16:20 -0400

    testing @downloadURL

--- OVERSEER/pick_skip.user.js
+++ OVERSEER/pick_skip.user.js
@@ -1,13 +1,29 @@
 // ==UserScript==
-// @name         [ OVERSEER ] LIVE PICK SKIP
-// @namespace    https://github.com/JeysonArtiles/amzn
-// @version      0.1
-// @description  TRACK PICK SKIPS IN REAL TIME.
+// @name         WebHook Test
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
 // @author       You
-// @match        https://*/*
-// @downloadURL  https://raw.githubusercontent.com/JeysonArtiles/amzn/master/overseer/pick_skip.user.js
-// @icon         https://www.google.com/s2/favicons?domain=amazon.com-update-test
+// @match        https://aftlite-na.amazon.com/wms/index
+// @icon         https://www.google.com/s2/favicons?domain=amazon.com
 // @grant        GM_xmlhttpRequest
 // @connect      chime.aws
-// @/require     https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
 // ==/UserScript==
+
+const name = prompt("Enter your name:");
+
+const trackActivity = async (action) => {
+
+}
+
+GM_xmlhttpRequest({
+    method: "POST",
+    url: "https://hooks.chime.aws/incomingwebhooks/0bdacf4c-58d1-4838-a9da-e743bc37a632?token=RDJpUDNkTk98MXxPMHh1b08wQnhxbmlKMThlS0RyMlFNVHBjZzg3ZmpxUF94OFl2RERSQ1Jr",
+    data: `{"Content":"Hello ${name}. Nice to see you."}`,
+    headers: {
+        "Content-Type": "application/json"
+    },
+    onload: function(response) {
+        console.log(response.responseText);
+    }
+});

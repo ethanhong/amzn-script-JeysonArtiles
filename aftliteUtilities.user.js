1a8d296bbac7220f31d1fce331ae6c1fe1b95355
Jeyson Artiles <JeysonArtiles@users.noreply.github.com>
2021-10-19 16:04:28 -0400

    Update aftliteUtilities.user.js

--- aftliteUtilities.user.js
+++ aftliteUtilities.user.js
@@ -2,7 +2,7 @@
 // @name         AFTLITE UTILITIES
 // @namespace    https://github.com/JeysonArtiles/amzn
 // @version      0.1
-// @description  Many scripts; Load ASIN Labor Track
+// @description  Many scripts; Load ASIN Labor Track / Get PA Number ASIN + Receive
 // @author       jeyartil
 // @match        https://aftlite-na.amazon.com/*
 // @icon         https://www.google.com/s2/favicons?domain=amazon.com

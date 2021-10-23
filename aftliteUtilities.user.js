864b650db48e593090864e2acbee16e70f702a5e
Jeyson Artiles <JeysonArtiles@users.noreply.github.com>
2021-10-23 16:46:37 -0400

    Update aftliteUtilities.user.js

--- aftliteUtilities.user.js
+++ aftliteUtilities.user.js
@@ -7,6 +7,7 @@
 // @match        https://aftlite-na.amazon.com/*
 // @icon         https://www.google.com/s2/favicons?domain=amazon.com
 // @grant        GM_xmlhttpRequest
+// @connect      chime.aws
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
 // @require      https://cdn.jsdelivr.net/npm/jsbarcode@3.11.0/dist/barcodes/JsBarcode.code128.min.js
@@ -28,6 +29,8 @@ hotkeys('shift+/', function() {
     //if (script == null) return;
 });
 */
+
+const AUTH_TOKEN = document.querySelector("meta[name=csrf-token]").content;
 
 const findPalletAsin = () => {
     const purchaseOrders = [...document.querySelectorAll(`#sortable-table-0 > tbody > tr`)];
@@ -163,42 +166,85 @@ const asinLaborTrack = () => {
 }
 
 const problemSolve = () => {
+    const ORDER_ID = [...document.querySelectorAll("#summary_table > tbody > tr")][0].children[1].innerText;
+
     const addToteForm = document.querySelector(`form[action="/wms/set_tote"]`);
     const addToteText = document.querySelector(`input[name="tote_code"]`);
     const addToteBtn = document.querySelector(`form[action="/wms/set_tote"]`);
-
     const addAsin = document.querySelector(`input[name="asin_or_upc"]`);
-    //
+
     const missing = [...document.querySelectorAll(`tr[valign="top"]`)];
 
-    //const test = document.querySelector(`form[action="/wms/finish_packing_picklist"]`);
-    //const barcodeDiv = document.createElement(`div`);
-
-    //barcodeDiv.innerHTMl = "TEST"
-
-    //test.appendChild(barcodeDiv)
     const totes = [...new Set([...[...document.querySelectorAll("a")].filter(a => a.innerText.includes("Sp"))].map(a => a.innerText))];
 
     addToteText.value = totes;
 
-    missing.map(item => {
-        const itm = {};
-        itm.location = item.cells[0];
-        itm.asin = item.cells[1];
-        itm.title = item.cells[2];
-        itm.weight = item.cells[3];
-        itm.picked = item.cells[4];
-        itm.packed = item.cells[5];
+    missing.map(itm => {
+        const item = {};
+        item.location = itm.cells[0];
+        item.asin = itm.cells[1];
+        item.title = itm.cells[2];
+        item.weight = itm.cells[3];
+        item.picked = itm.cells[4];
+        item.packed = itm.cells[5];
 
-        const itemName = itm.title.innerText;
-        const itemAsin = itm.asin.innerText;
+        const realWeight = item.weight.innerText;
+        item.weight.innerText = item.location.innerText.split("(")[0];
+
+        let FALSE_SKIP_LOCATION = item.weight.innerText;
+
+        if (item.location.innerText.includes("skipped")) {
+            const cubiscanBtn = [...document.querySelectorAll("button")].filter(btn => btn.innerText.includes("Cubiscan"));
+
+            const missingItemCubiscanBtn = cubiscanBtn.filter(btn => btn.previousSibling.previousSibling.value == item.asin.innerText )[0];
+
+            //missingItemCubiscanBtn.innerHTML += "<button href='#'>False Skip</button>";
+            item.location.innerHTML += "<br><button id='falseSkipBtn'>False Skip</button>";
+
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: `https://aftlite-na.amazon.com/labor_tracking/lookup_history?utf8=%E2%9C%93&authenticity_token=${AUTH_TOKEN}&tote_code=${totes}`,
+                onload: async (response) => {
+                    const PAGE = new DOMParser().parseFromString(response.responseText, "text/html");
+                    const TABLE = [...PAGE.querySelectorAll(".reportLayout > tbody > tr")];
+                    const PICKER = TABLE[TABLE.length - 1].cells[12].innerText;
+
+                    const falseSkipBtn = document.querySelector("#falseSkipBtn");
+
+                    const FALSE_SKIP_LOG = `${PICKER.toUpperCase()} => ${item.asin.innerText} @ ${FALSE_SKIP_LOCATION.trim()} (${item.title.innerText})`;
+
+                    falseSkipBtn.addEventListener("click", () => {
+                        GM_xmlhttpRequest({
+                            method: "POST",
+                            url: "https://hooks.chime.aws/incomingwebhooks/a31525dd-151d-423f-a04b-ec74189d9506?token=VDBJbndTVVN8MXxjWE9vcTZ6VlhTblhXdGFfNTRRY2QtdkN4VXZxc2dwTnNNZWljX1dLSU1j",
+                            data: `{"Content":"${FALSE_SKIP_LOG}"}`,
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            onload: function(response) {
+                                //console.log(response.responseText);
+                            }
+                        });
+
+                        falseSkipBtn.hidden = true;
+                    })
+
+
+
+                    item.weight.innerText = realWeight;
+                }
+            });
+
+        }
+
+        const itemName = item.title.innerText;
+        const itemAsin = item.asin.innerText;
 
         const copyAsin = () => { addAsin.value = itemAsin };
 
-        itm.title.addEventListener("click", () => copyAsin());
-        itm.title.innerHTML = `<a href="#">${itemName}</a>`;
+        item.title.addEventListener("click", () => copyAsin());
+        item.title.innerHTML = `<a href="#">${itemName}</a>`;
     });
-
 }
 
 
@@ -217,3 +263,5 @@ if (location.pathname.includes("/inventory/view_inventory_for_asin")) {
 if (location.pathname.includes("/wms/pack_by_picklist")) {
     problemSolve();
 }
+
+

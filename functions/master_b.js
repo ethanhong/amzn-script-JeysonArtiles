25070564f4a47eb4cfd5d80ebc2f6bd5579f26ae
Jeyson Artiles <JeysonArtiles@users.noreply.github.com>
2022-04-25 10:08:33 -0400

    Update and rename master_a.js to master_b.js

--- functions/master_b.js
+++ functions/master_b.js
@@ -15,14 +15,14 @@ parse.items = (domElement) => {
     if (qty.includes("item")) return Number(qty.split(" (")[1].split(" i")[0]);
 }
 
-parse.html = (URL, QUERY) => {
+parse.html = (URL, ELEMENT, QUERY) => {
     GM_xmlhttpRequest({
         method: "GET",
         url: URL,
         onload: async (response) => {
             const PAGE = new DOMParser().parseFromString(response.responseText, "text/html");
 
-            html.innerHTML = PAGE.querySelector(QUERY).innerHTML;
+            ELEMENT.innerHTML = PAGE.querySelector(QUERY).innerHTML;
             return PAGE
         }
     });

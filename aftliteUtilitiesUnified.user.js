660488c7b063da594c11535af71285eb7310793b
Jeyson Artiles <JeysonArtiles@users.noreply.github.com>
2022-05-20 11:22:40 -0400

    Update aftliteUtilitiesUnified.user.js

--- aftliteUtilitiesUnified.user.js
+++ aftliteUtilitiesUnified.user.js
@@ -313,7 +313,7 @@ if (location.pathname.includes("/labor_tracking/labor_summary")) {
     rateReport();
 }
 
-const autoLog = () => {
+const ' = () => {
     const login = localStorage.getItem("logged_user").toLowerCase();
     const password = localStorage.getItem("password") == null && prompt("Enter Password to enable Auto-Login") || localStorage.getItem("password");
 
@@ -334,8 +334,7 @@ const autoLog = () => {
 }
 
 if (location.pathname.includes("signin")) {
-    localStorage.getItem("logged_user") == "JEYARTIL" 
-    autoLog();
+    localStorage.getItem("logged_user") == "JEYARTIL" && autoLog();
 }
 
 const pickAdmin = () => {
